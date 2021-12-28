9e7ce61170b1df4c9af582ab7fb9a40fa89c803f
Oleg <example@gmail.com>
2021-12-29 00:14:11 +0200

    29.12.21 first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+'use srtict';
+
+//alert('Hello World!');
+//const result = confirm('Are you here?');
+//console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '');
+
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your surname?', '');
+// answers[2] = prompt('What is your age?', '');
+// console.log(typeof answers);
+//document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}`);
+
+// let user = 'Oleg';
+// console.log(`Hello, ${user}!`);
+
+// let incr = 10,
+//   decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(4 * 3 != 8);
+
+// const isChecked = false,
+//   isClosed = false;
+// console.log(isChecked || !isClosed);
